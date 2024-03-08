98c3bc361f60ea2d535f26451e911f00fec142e1
Aleksandr <plombiriny@gmail.com>
2024-03-08 15:07:09 +0200

    add README.md, add requirements.txt

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# pars-HDrezka
+# Project pars-HDrezka
+
+Pars-HDrezka is a script for gathering information from the HDrezka website, specifically from the "Latest Releases" section.
+
+## Installing
+
+1. Clone the repository from Git Hub: 
+    `git clone https://github.com/Alexandr1123212545/pars-HDrezka.git`
+2. Create virtual environment: 
+   `python -m venv "your_venv"`
+3. Install dependencies:
+   `pip install -r requirements.txt`
+4.  Determine the list of pages, specifying the first and last pages in the script:
+       ```
+    FIRST_PAGE = 'first page'
+    LAST_PAGE = 'last page'
+    ```
+5. Run script!
+
+
+As a result, you get a JSON file. Each block describes one movie and contains information about its title, release year, country of production, genre, and a link to the movie's page on the website.
---
+++ main.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+from time import sleep
+import json
+
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+page = 1
+
+for page in range(1, 10):
+    url = f'https://rezka.ag/films/page/{page}/'
+    r = requests.get(url, headers=headers)
+    sleep(3)
+
+    soup = BeautifulSoup(r.text, 'lxml')
+    films = soup.findAll('div', class_='b-content__inline_item-link')
+
+    for film in films:
+        ycg = film.find('div').text.split(',') # Year Country Genre
+
+        data = {
+                'title': film.find('a').text,
+                'year': ycg[0],
+                'country': ycg[1].lstrip(),
+                'genre': ycg[2].lstrip(),
+                'link': film.find('a').get('href'),
+            }
+            
+        with open('movies.json', 'a', encoding='utf-8') as json_file:
+            json.dump(data, json_file, ensure_ascii=False, indent=4)
+            json_file.write('\n')
+
+    print(f'Page {page} ready!')
+print('Done!')
+
